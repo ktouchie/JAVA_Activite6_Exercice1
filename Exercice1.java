ccc00feb5d9e8b8e214311c559f6fb809fbbd640
Karin Touchie <ktouchie@gmail.com>
2015-12-14 17:03:44 +0100

    Write prompts and test method

--- Exercice1.java
+++ Exercice1.java
@@ -2,21 +2,23 @@ import java.util.*;
 class Exercice1 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        System.out.println("Enter x:");
-        int x = sc.nextInt();
-        System.out.println("Enter y:");
-        int y = sc.nextInt();
-        System.out.println("Before...");
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
-        int a = x;
-        x = y;
-        y = a;
-        System.out.println("After...");
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
-
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a value for X:");
+        String x = sc.nextLine();
+        
+        System.out.println("Enter a value for Y:");
+        String y = sc.nextLine();
+        
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        
+        swap(x,y);
+        
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        
 	}
 
     public static void swap(String A, String B) {
